d28395983a637be8c888a266f7524f5ddfbf744e
AO Industries, Inc. <support@aoindustries.com>
2016-09-22 04:05:54 -0500

    Avoiding calling writeTo when capturing.

--- src/main/java/com/semanticcms/core/model/ElementWriter.java
+++ src/main/java/com/semanticcms/core/model/ElementWriter.java
@@ -30,9 +30,14 @@ import java.io.Writer;
 public interface ElementWriter {
 
 	/**
+	 * <p>
 	 * Writes the element into the given writer.
 	 * This should include everything added before the body, the body itself,
 	 * and everything after after the body.
+	 * </p>
+	 * <p>
+	 * For efficiency, this should not be called when the output would otherwise be discarded.
+	 * </p>
 	 */
 	void writeTo(Writer out, ElementContext context) throws Exception;
 }
