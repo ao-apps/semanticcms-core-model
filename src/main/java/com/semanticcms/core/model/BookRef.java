b3029ac4b242478278d1d9877c7f16cacff3e182
AO Industries, Inc. <support@aoindustries.com>
2017-07-31 21:04:06 -0500

    New BookRef class to refer to a book by both domain and name.

---
+++ src/main/java/com/semanticcms/core/model/BookRef.java
@@ -0,0 +1,120 @@
+/*
+ * semanticcms-core-model - Java API for modeling web page content and relationships.
+ * Copyright (C) 2014, 2015, 2016, 2017  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-core-model.
+ *
+ * semanticcms-core-model is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-core-model is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-core-model.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.semanticcms.core.model;
+
+import com.aoindustries.lang.NullArgumentException;
+
+/**
+ * A book reference contains a domain and a book name.
+ */
+public class BookRef {
+
+	private final String domain;
+
+	private final String name;
+
+	public BookRef(String domain, String bookName) {
+		this.domain = NullArgumentException.checkNotNull(domain, "domain");
+		this.name = NullArgumentException.checkNotNull(bookName, "name");
+		if(!this.name.startsWith("/")) throw new IllegalArgumentException("Book name does not begin with a slash: " + this.name);
+	}
+
+	/**
+	 * Gets the domain of this book.  Two books are considered equal when they
+	 * are in the same domain and have the same name.
+	 */
+	public String getDomain() {
+		return domain;
+	}
+
+	/**
+	 * The name of the book this refers to.
+	 * This will always begin with a slash (/).
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * The prefix of the book this refers to.
+	 * This will be <code>""</code> for the root book <code>"/"</code>.
+	 */
+	public String getPrefix() {
+		String bn = name;
+		return "/".equals(bn) ? "" : bn;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) return true;
+		if(!(obj instanceof BookRef)) return false;
+		BookRef other = (BookRef)obj;
+		return
+			domain.equals(other.domain)
+			&& name.equals(other.name)
+		;
+	}
+
+	private int hash;
+
+	@Override
+	public int hashCode() {
+		int h = hash;
+		if(h == 0) {
+			h = domain.hashCode();
+			h = h * 31 + name.hashCode();
+			hash = h;
+		}
+		return h;
+	}
+
+	/**
+	 * Ordered by domain, name.
+	 *
+	 * @see  #getServletPath()
+	 */
+	public int compareTo(BookRef o) {
+		int diff = domain.compareTo(o.domain);
+		if(diff != 0) return diff;
+		return name.compareTo(o.name);
+	}
+
+	@Override
+	public String toString() {
+		int domainLen = domain.length();
+		if(domainLen == 0) {
+			return name;
+		} else {
+			return
+				new StringBuilder(
+					domainLen
+					+ 1 // ':'
+					+ name.length()
+				)
+				.append(domain)
+				.append(':')
+				.append(name)
+				.toString();
+		}
+	}
+}
